04be6794f7808e438b2d8cdb26b658f4dcf7727f
MuriloQuintal <130596582+MuriloQuintal@users.noreply.github.com>
2023-04-12 17:02:54 -0300

    vfxcvfx
    dtfgtfcdgt

---
+++ Verificando ordenação.py
@@ -0,0 +1,7 @@
+val1 = int(input("Insira o valor 1: "))
+val2 = int(input("Insira o valor 2: "))
+val3 = int(input("Insira o valor 3: "))
+if val1 < val2 and val2 < val3:
+    print("crescente")
+else:
+    print("não está em ordem crescente")
